a003263ad4b7df1b297a5c639bffc968122005a9
Abiodun Sanni <abbisco.ae@gmail.com>
2024-08-24 15:50:59 +0100

    modified el script with cleaned cols**

--- scripts/python_el.py
+++ scripts/python_el.py
@@ -28,59 +28,59 @@ os.chdir("./Data")
 
 autocheck_data = os.listdir() # This assigns the list data objects available in your directory to the variable on the left
 existing_tables = inspector.get_table_names()
+connection = engine.connect()
 for data in autocheck_data: # loops through each data objects (CSVs)
-    with engine.connect() as connection:
-        connection.execute(text(f"USE DATABASE AUTOCHECK"))
-        if data[:-4] not in existing_tables:
-            df = pd.read_csv(data) # reads the data as pandas dataframe
-            df.columns = df.columns.str.lower().str.replace(' ', '_')
-            print(df.head(2)) # prints the first 2 rows of the data
-            print(f'Loading {data[:-4]} into snowflake') # prints a log to the console to show what the program is doing at the moment
-            """
-                The code below loads each of the data object to the specified location as specified in your configuration
-            """
-            df.to_sql(f'{data[:-4]}', \
-                con=connection, \
-                schema = 'RAW', \
-                if_exists = 'replace', \
-                index = False, method='multi')
-        else:
-            df = pd.read_csv(data) # reads the data as pandas dataframe
-            df.columns = df.columns.str.lower().str.replace(' ', '_')
-            print(df.head(2)) # prints the first 2 rows of the data
-            print(f'Loading temp_{data[:-4]} into snowflake') # prints a log to the console to show what the program is doing at the moment
-            df.to_sql(f'temp_{data[:-4]}', \
-                con=connection, \
-                schema = 'RAW', \
-                if_exists = 'replace', \
-                index = False, method='multi')
-            print(f'Loaded temp_{data[:-4]} object to the db')
+    connection.execute(text(f"USE DATABASE AUTOCHECK"))
+    if data[:-4] not in existing_tables:
+        df = pd.read_csv(data) # reads the data as pandas dataframe
+        df.columns = df.columns.str.lower().str.replace(' ', '_')
+        print(df.head(2)) # prints the first 2 rows of the data
+        print(f'Loading {data[:-4]} into snowflake') # prints a log to the console to show what the program is doing at the moment
+        """
+            The code below loads each of the data object to the specified location as specified in your configuration
+        """
+        df.to_sql(f'{data[:-4]}', \
+            con=connection, \
+            schema = 'RAW', \
+            if_exists = 'replace', \
+            index = False, method='multi')
+    else:
+        df = pd.read_csv(data) # reads the data as pandas dataframe
+        df.columns = df.columns.str.lower().str.replace(' ', '_')
+        print(df.head(2)) # prints the first 2 rows of the data
+        print(f'Loading temp_{data[:-4]} into snowflake') # prints a log to the console to show what the program is doing at the moment
+        df.to_sql(f'temp_{data[:-4]}', \
+            con=connection, \
+            schema = 'RAW', \
+            if_exists = 'replace', \
+            index = False, method='multi')
+        print(f'Loaded temp_{data[:-4]} object to the db')
+        
+        if data[:-4].lower() == 'borrower_data':
+            target_id = 'borrower_id'
+        elif data[:-4].lower() == 'loan_data':
+            target_id = 'loan_id'
+        elif data[:-4].lower() == 'repayment_data':
+            target_id = 'payment_id'
+        elif data[:-4].lower() == 'schedule_data':
+            target_id = 'schedule_id'
+        cols = []
+        for col in df.columns:
+            cols.append('source.'+col)
             
-            if data[:-4].lower() == 'borrower_data':
-                target_id = 'borrower_id'
-            elif data[:-4].lower() == 'loan_data':
-                target_id = 'loan_id'
-            elif data[:-4].lower() == 'repayment_data':
-                target_id = 'payment_id'
-            elif data[:-4].lower() == 'schedule_data':
-                target_id = 'schedule_id'
-            cols = []
-            for col in df.columns:
-                cols.append('source.'+col)
-                
-            src_cols = ', '.join(cols)
-            columns = ', '.join(df.columns)
-            merge_sql = f"""
-                        MERGE INTO RAW.{data[:-4]} AS target
-                        USING RAW.temp_{data[:-4]} AS source
-                        ON target.{target_id} = source.{target_id}
-                        WHEN NOT MATCHED THEN
-                            INSERT ({columns}) VALUES ({src_cols});
-                        """
-            connection.execute(text(merge_sql))
-            print(f'Merged temp_{data[:-4]} object into {data[:-4]} object')
+        src_cols = ', '.join(cols)
+        columns = ', '.join(df.columns)
+        merge_sql = f"""
+                    MERGE INTO RAW.{data[:-4]} AS target
+                    USING RAW.temp_{data[:-4]} AS source
+                    ON target.{target_id} = source.{target_id}
+                    WHEN NOT MATCHED THEN
+                        INSERT ({columns}) VALUES ({src_cols});
+                    """
+        connection.execute(text(merge_sql))
+        print(f'Merged temp_{data[:-4]} object into {data[:-4]} object')
 
-            connection.execute(text(f"DROP TABLE IF EXISTS RAW.temp_{data[:-4]}"))
+        connection.execute(text(f"DROP TABLE IF EXISTS RAW.temp_{data[:-4]}"))
 
-    connection.close()
+connection.close()
 print('Loaded all data objects to the db')
